a53c64ce81acd7b699863425721f6362433a2ef9
v-Vahe <vabarseg@gmail.com>
2022-04-15 22:46:32 -0700

    added validations

--- database/initialize_db.py
+++ database/initialize_db.py
@@ -4,7 +4,8 @@ import os
 import pandas as pd
 from sql_server.sql_utils import (
     create_schema, 
-    df_to_sql, 
+    df_to_sql,
+    drop_tables, 
     get_identity_tables, 
     set_identity_insert_on, 
     set_identity_insert_off, 
@@ -36,37 +37,41 @@ def read_table(table):
 
 def main():
 
-    create_schema()
     
     ordered_tables_list = table_list()
-    # identify the tables with identity constraint
     identity_tables = get_identity_tables()
     
     for table in ordered_tables_list:
-
+        
         table = os.path.join('database/initial_csv_data',table)
         table_name = os.path.basename(table)[:-4]
 
-        print(f"\n populating {table_name} ...")
+        # check if the table is already populated
+        if table_exists(table_name): 
+            response = input(
+                "Do you want to drop the tables and repopulate? [Y]/[N]"
+                )
+            print(response)
+            if response.upper() == 'N':
+                exit()
+            if response.upper() == 'Y':
+                drop_tables()
+                create_schema()
+            else:
+                print('invalid response')
+
+        if table_name in identity_tables:
+                set_identity_insert_on(table_name)
 
         # load csv data into a dataframe
         df = read_table(table)
-
-        # set identity insert on
-        if table_name in identity_tables:
-                set_identity_insert_on(table_name)
-    
-        # check if the table is already populated
-        if table_exists(table_name) == True: 
-            continue
-
+        
         # clean the dataframe / transform
         df = df.replace([r'\N'],'')
     
         # append to sql table
         df_to_sql(df, table_name)
-
-        # set identity_insert OFF for safety
+        
         if table_name in identity_tables:
             set_identity_insert_off(table_name)
     
